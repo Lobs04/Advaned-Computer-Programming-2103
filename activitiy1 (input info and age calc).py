c700624f0730ee1335ad3c7363f4277cc6212a15
Libs <131282529+Lobs04@users.noreply.github.com>
2023-09-21 21:00:45 -0700

    Add files via upload

---
+++ activitiy1 (input info and age calc).py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+addy = input("Enter your address: ")
+print("Your name is", name ,"you're", age ,"years old and currently living in", addy)
+
+byear = eval(input("Enter your birth year: "))
+cyear = 2023
+tyear = cyear - byear
+print("Your age is: ", tyear)
---
+++ activity4.py
@@ -0,0 +1,45 @@
+print("1.)")
+print("Hello peeps!")
+
+print("2.)")
+print("This is from a line of code")
+
+print("3.)")
+q, w = 5, 5.5
+print(q)
+print(w)
+print("Hello, World!")
+
+print("4.)")
+x, y = 78, 1
+print(x+y, "(sum)")
+
+print("5.)")
+a = 7
+product = 5*a
+print(product)
+
+print("6.)")
+b, c, d = 2, 11, 0
+b, c = b*2, c+(d*3)
+d = b*c
+print(d)
+
+print("7.)")
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+tnum = int(num1) + int(num2)
+print("your sum is: ", tnum)
+
+print("8.)")
+sqred, cubed = 3**2, 3**3
+print(sqred, cubed)
+
+print("9.)")
+name = "Gly"
+print(name)
+
+print("10.)")
+Apple_price = 20
+Orange_price = 20
+print(Apple_price == Orange_price)
